bec63743e80b40cb04484c5cc6edca9f65c5f4b9
vrogova11 <rogova_veronika_alexandrovna@mail.ru>
2020-06-13 11:40:00 +0300

    Практическое задание № 1

---
+++ dz1.py
@@ -0,0 +1,24 @@
+numb_1 = 5
+numb_2 = 34.25
+name_1 = 'Елена'
+name_2 = 'Молчанова'
+
+print(numb_1)
+print(numb_2)
+print(name_1)
+print(name_2)
+print('')
+
+name = input('Введите имя: ')
+surname = input('Введите фамилию: ')
+age = (input('Введите ваш возраст: '))
+weight = (input('Введите ваш вес: '))
+print('')
+
+print('Ваша анкета:')
+print(f'Имя: {name}')
+print(f'Фамилия: {surname}')
+print(f'Возраст: {age}')
+print(f'Вес: {weight}')
+
+

706204a1dfdd7fd99f96184f782bfe94ac31768c
vrogova11 <rogova_veronika_alexandrovna@mail.ru>
2020-06-20 15:37:52 +0300

    Lesson-3

--- dz4.py
+++
@@ -1,7 +0,0 @@
-my_str = input('Введите строку из нескольких слов: ')
-my_list = []
-for i in my_str.split():
-    my_list.append(i)
-
-for i, val in enumerate(my_list, 1):
-    print(f'{i}) {val[:10]}')
